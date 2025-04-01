2404d15350f05746d2177f4c26cd69dcd63f6ba6
Elvin-Mworia <elvinmworia@gmail.com>
2025-04-02 01:48:50 +0300

    user schema

---
+++ model/user.js
@@ -0,0 +1,53 @@
+// models/User.js
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, 'First name is required'],
+    trim: true,
+    maxlength: [50, 'First name cannot be more than 50 characters']
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last name is required'],
+    trim: true,
+    maxlength: [50, 'Last name cannot be more than 50 characters']
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [8, 'Password must be at least 8 characters'],
+    select: false // This ensures the password isn't returned in queries unless explicitly asked for
+  },
+  passwordChangedAt: Date,
+  role: {
+    type: String,
+    enum: ['admin','user','staff'],
+    default: 'user'
+  },
+  active: {
+    type: Boolean,
+    default: true,
+    select: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
