7e82c5c821b95314a7412cd58f0bf7da05b565d8
Elvin-Mworia <elvinmworia@gmail.com>
2025-04-09 10:08:57 +0300

    remove white spaces from the plaintext password

--- utils/hashing.js
+++ utils/hashing.js
@@ -4,9 +4,11 @@ const  User=require("../model/user.js");
 const bcrypt = require('bcryptjs');
 // Middleware to hash password before saving
 function hashingPassword(req,res,next){
+    if(req.body.password.trim()<=0){
+        return res.status(400).json({ message: "Password is required" });
+    }
     // Hash the password with cost of 12
-    console.log(req.body.password)
-    bcrypt.hash(req.body.password, 12, function(err, hash) {
+    bcrypt.hash(req.body.password.trim(), 12, function(err, hash) {
         if (err) {
             return res.status(500).json({ message: "Internal server error" });
         }
